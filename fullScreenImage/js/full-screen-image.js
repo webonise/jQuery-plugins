ba91387254acd9a1a555de216749df33845f93fe
swapnil vathare <swapnil@weboniselab.com>
2013-08-22 15:02:25 +0530

    position prefrence added

--- fullScreenImage/js/full-screen-image.js
+++ fullScreenImage/js/full-screen-image.js
@@ -1,5 +1,5 @@
 /*
- *  full Screen Image 1.1 - jQuery plugin
+ *  full Screen Image 1.2 - jQuery plugin
  *	written by Weboniselab
  *	http://www.webonise.com
  *
@@ -9,171 +9,171 @@
  *	http://jquery.com
  *
  */
-(function($) {
+(function ($) {
     "use strict";
     var obj = $([]);
     var tempOptions;
-    $(window).bind('resize.fullScreenImage', function(){
-        if (obj.length){
+    $(window).bind('resize.fullScreenImage', function () {
+        if (obj.length) {
             obj.fullScreenImage();
         }
     });
-    $('img.fsi').load(function(){
-        if (obj.length){
-        obj.fullScreenImage();
+    $('img.fsi').load(function () {
+        if (obj.length) {
+            obj.fullScreenImage();
         }
     });
-    $.fn.fullScreenImage = function(options){
-        if(typeof options === 'undefined'){
+    $.fn.fullScreenImage = function (options) {
+        if (typeof options === 'undefined') {
             options = tempOptions;
         }
         else {
             tempOptions = options;
         }
 
-    var defaults = {
-        imageHeight:        1000,
-        imageWidth:         1600,
-        verticalSpace:      0,
-        horizontalSpace:    0,
-        verticalPosition:   'center',
-        horizontalPosition:   'center'
-    };
-    options = $.extend(defaults,options);
-    this.each(function(){
-    obj = $(this);
-    var fullScreenImage = $('img.fsi');
-    var windowHeight = $(window).height();
-    var windowWidth = $(window).width();
-    var objHeight = windowHeight - options.verticalSpace;
-    var objWidth = windowWidth - options.horizontalSpace;
-    var heightDifference;
-    var widthDifference;
-    var imageNewWidth;
-    var imageNewHeight;
-    var imageTopMinusMargin;
-    var imageLeftMinusMargin;
-    obj.css({
-        height:objHeight,
-        width:objWidth,
-        position: 'relative',
-        overflow: 'hidden'
-    });
-    fullScreenImage.css({
-        position: 'absolute',
-        maxWidth: 'none',
-        maxHeight:'none'
-    });
-    if(options.imageHeight <= windowHeight){
-        heightDifference = windowHeight - options.imageHeight;
-    }else{
-        heightDifference = options.imageHeight - windowHeight;
-    }
-    if(options.imageWidth <= windowWidth){
-        widthDifference = windowWidth - options.imageWidth;
-    }else{
-        widthDifference = options.imageWidth - windowWidth;
-    }
-    if(widthDifference <= heightDifference){
-        imageNewWidth = (windowHeight / options.imageHeight)*options.imageWidth;
-        if(imageNewWidth < windowWidth){
-            imageNewHeight = (windowWidth / options.imageWidth)*options.imageHeight;
-            imageTopMinusMargin = -((imageNewHeight - windowHeight)/2);
-
-            fullScreenImage.css({
-                height:imageNewHeight,
-                width:windowWidth,
-                top:imageTopMinusMargin,
-                left:0
+        var defaults = {
+            imageHeight:1000,
+            imageWidth:1600,
+            verticalSpace:0,
+            horizontalSpace:0,
+            verticalPosition:'center',
+            horizontalPosition:'center'
+        };
+        options = $.extend(defaults, options);
+        this.each(function () {
+            obj = $(this);
+            var fullScreenImage = $('img.fsi');
+            var windowHeight = $(window).height();
+            var windowWidth = $(window).width();
+            var objHeight = windowHeight - options.verticalSpace;
+            var objWidth = windowWidth - options.horizontalSpace;
+            var heightDifference;
+            var widthDifference;
+            var imageNewWidth;
+            var imageNewHeight;
+            var imageTopMinusMargin;
+            var imageLeftMinusMargin;
+            obj.css({
+                height:objHeight,
+                width:objWidth,
+                position:'relative',
+                overflow:'hidden'
             });
-            if(!(options.verticalPosition == 'center')){
-
-                if(options.verticalPosition == 'top'){
-                    fullScreenImage.css({
-                        top:0
-                    });
-                }else if(options.verticalPosition == 'bottom'){
-                    fullScreenImage.css({
-                        top: 'auto',
-                        bottom:0
-                    });
-                }
+            fullScreenImage.css({
+                position:'absolute',
+                maxWidth:'none',
+                maxHeight:'none'
+            });
+            if (options.imageHeight <= windowHeight) {
+                heightDifference = windowHeight - options.imageHeight;
+            } else {
+                heightDifference = options.imageHeight - windowHeight;
             }
+            if (options.imageWidth <= windowWidth) {
+                widthDifference = windowWidth - options.imageWidth;
+            } else {
+                widthDifference = options.imageWidth - windowWidth;
+            }
+            if (widthDifference <= heightDifference) {
+                imageNewWidth = (windowHeight / options.imageHeight) * options.imageWidth;
+                if (imageNewWidth < windowWidth) {
+                    imageNewHeight = (windowWidth / options.imageWidth) * options.imageHeight;
+                    imageTopMinusMargin = -((imageNewHeight - windowHeight) / 2);
 
-        }else{
-            imageLeftMinusMargin = -((imageNewWidth - windowWidth)/2);
-            fullScreenImage.css({
-                height:windowHeight,
-                width:imageNewWidth,
-                left:imageLeftMinusMargin,
-                top:0
-            });
-
-            if(!(options.horizontalPosition == 'center')){
-
-                if(options.horizontalPosition == 'left'){
                     fullScreenImage.css({
+                        height:imageNewHeight,
+                        width:windowWidth,
+                        top:imageTopMinusMargin,
                         left:0
                     });
-                }else if(options.horizontalPosition == 'right'){
-                    fullScreenImage.css({
-                        left: 'auto',
-                        right:0
-                    });
-                }
-            }
-        }
-    }else{
-        imageNewHeight = (windowWidth / options.imageWidth)*options.imageHeight;
-        if(imageNewHeight < windowHeight){
-            imageNewWidth = (windowHeight / options.imageHeight)*options.imageWidth;
-            imageLeftMinusMargin = -((imageNewWidth - windowWidth)/2);
-            fullScreenImage.css({
-                height:windowHeight,
-                width:imageNewWidth,
-                left:imageLeftMinusMargin,
-                top:0
-            });
+                    if (!(options.verticalPosition == 'center')) {
 
-            if(!(options.horizontalPosition == 'center')){
+                        if (options.verticalPosition == 'top') {
+                            fullScreenImage.css({
+                                top:0
+                            });
+                        } else if (options.verticalPosition == 'bottom') {
+                            fullScreenImage.css({
+                                top:'auto',
+                                bottom:0
+                            });
+                        }
+                    }
 
-                if(options.horizontalPosition == 'left'){
+                } else {
+                    imageLeftMinusMargin = -((imageNewWidth - windowWidth) / 2);
                     fullScreenImage.css({
-                        left:0
-                    });
-                }else if(options.horizontalPosition == 'right'){
-                    fullScreenImage.css({
-                        left: 'auto',
-                        right:0
-                    });
-                }
-            }
-
-        }else{
-            imageTopMinusMargin = -((imageNewHeight - windowHeight)/2);
-            fullScreenImage.css({
-                height:imageNewHeight,
-                width:windowWidth,
-                top:imageTopMinusMargin,
-                left:0
-            });
-            if(!(options.verticalPosition == 'center')){
-
-                if(options.verticalPosition == 'top'){
-                    fullScreenImage.css({
+                        height:windowHeight,
+                        width:imageNewWidth,
+                        left:imageLeftMinusMargin,
                         top:0
                     });
-                }else if(options.verticalPosition == 'bottom'){
+
+                    if (!(options.horizontalPosition == 'center')) {
+
+                        if (options.horizontalPosition == 'left') {
+                            fullScreenImage.css({
+                                left:0
+                            });
+                        } else if (options.horizontalPosition == 'right') {
+                            fullScreenImage.css({
+                                left:'auto',
+                                right:0
+                            });
+                        }
+                    }
+                }
+            } else {
+                imageNewHeight = (windowWidth / options.imageWidth) * options.imageHeight;
+                if (imageNewHeight < windowHeight) {
+                    imageNewWidth = (windowHeight / options.imageHeight) * options.imageWidth;
+                    imageLeftMinusMargin = -((imageNewWidth - windowWidth) / 2);
                     fullScreenImage.css({
-                        top: 'auto',
-                        bottom:0
+                        height:windowHeight,
+                        width:imageNewWidth,
+                        left:imageLeftMinusMargin,
+                        top:0
                     });
+
+                    if (!(options.horizontalPosition == 'center')) {
+
+                        if (options.horizontalPosition == 'left') {
+                            fullScreenImage.css({
+                                left:0
+                            });
+                        } else if (options.horizontalPosition == 'right') {
+                            fullScreenImage.css({
+                                left:'auto',
+                                right:0
+                            });
+                        }
+                    }
+
+                } else {
+                    imageTopMinusMargin = -((imageNewHeight - windowHeight) / 2);
+                    fullScreenImage.css({
+                        height:imageNewHeight,
+                        width:windowWidth,
+                        top:imageTopMinusMargin,
+                        left:0
+                    });
+                    if (!(options.verticalPosition == 'center')) {
+
+                        if (options.verticalPosition == 'top') {
+                            fullScreenImage.css({
+                                top:0
+                            });
+                        } else if (options.verticalPosition == 'bottom') {
+                            fullScreenImage.css({
+                                top:'auto',
+                                bottom:0
+                            });
+                        }
+                    }
                 }
             }
-        }
-    }
-    return obj;
-    });
+            return obj;
+        });
 
     };
 })(jQuery);
